4f657208c46251a13a0d709dbdbdc49c400fc60d
briana wynn <briana.wynn@revature.net>
2021-08-17 20:02:48 -0700

    add demoFile and first program

---
+++ week1/HelloCshapr/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HelloCshapr
+{
+    class Program
+    {
+      //Build a simple calculator using 5 methods: Add, Multiply, subtract, divide, print, 
+       
+        public double add(double a, double b){
+         return: a + b; 
+
+        }
+
+        public double subtract(double a, double b){
+          return: a -b; 
+        }
+
+        public double multiply(double a, double b){
+          return a*b;
+        }
+        public double divide(double a, double b){
+          return a/b; 
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Winnie!");
+            Console.ReadLine();
+        }
+    }
+}
