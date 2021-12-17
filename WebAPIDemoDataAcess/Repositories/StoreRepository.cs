de903e9d7d71bcf4316446371e4365c5d43abc5c
briana.wynn@revature.net <briana.wynn@revature.net>
2021-12-16 19:07:41 -0800

    Update store repository to base crud and dependency injection

--- WebAPIDemoDataAcess/Repositories/StoreRepository.cs
+++ WebAPIDemoDataAcess/Repositories/StoreRepository.cs
@@ -4,21 +4,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WebAPIDemoBusinessLayer.Interfaces;
 using WebAPIDemoDataAcess.EntityModels;
 
-namespace WebAPIDemoBusinessLayer.Repositories
+namespace WebAPIDemoDataAcess.Repositories
 {
-    public class StoreRepository: IStoreRepository 
+    public class StoreRepository
     {
-        private readonly CoreDbContext __dbContext;
-
-        
-
+        private readonly CoreDbContext _context;
 
         public StoreRepository(CoreDbContext dbContext) {
 
-            __dbContext = dbContext;
+            _context = dbContext;
         }
 
         public StoreRepository() { }
@@ -27,82 +23,164 @@ namespace WebAPIDemoBusinessLayer.Repositories
 
       
         
-        public List<Store> AllStore()
+        public List<Store> GetAllStores()
         {
-            // convert to viewCustomers. This would be for formatting responsiblities
-            //for demo, just return the entire customer
+            List<Store> lstStore = null;
+            try
+            {
+                lstStore = _context.Stores.ToList();
+            }
+            catch (Exception)
+            {
+                lstStore = null;
+            }
 
-            List<Store> entityStore = _dbContext.Stores.FromSqlInterpolated($"SELECT * from Stores").ToList();
-
-            //go add the entity model
-            //entity model added
-            //switch to getting an Inventory
-            //Console.WriteLine(entityStore[0].Address);
-
-
-            return entityStore; //count = 21 when you check the infomration is there
+            return lstStore;
         }
 
-        public List<ViewStoreOrder> StoreOrder()
+        public Store GetStore(Store storeObj)
         {
-            // convert to viewCustomers. This would be for formatting responsiblities
-            //for demo, just return the entire customer
+            Store store = null;
+            try
+            {
+                store = _context.Stores.Find(storeObj.StoreId);
 
-            List<ViewStoreOrder> entityStoreOrder = _dbContext.ViewStoreOrder.FromSqlInterpolated($"SELECT * from ViewStoreOrder").ToList();
+            }
+            catch (Exception)
+            {
+                store = null;
+            }
 
-            //go add the entity model
-            //entity model added
-            //switch to getting an Inventory
-            Console.WriteLine(entityStoreOrder[0].OrderId);
-
-
-            return entityStoreOrder; 
+            return store;
         }
 
-        public List<ViewStoreOrder> StoreOrder(int num)
+        public bool AddStore(Store store)
         {
-            // convert to viewCustomers. This would be for formatting responsiblities
-            //for demo, just return the entire customer
-
-            List<ViewStoreOrder> entityStoreOrder = _dbContext.ViewStoreOrder.FromSqlInterpolated($"SELECT * from ViewStoreOrder where storeId = {num}").ToList();
-
-            //go add the entity model
-            //entity model added
-            //switch to getting an Inventory
-            if (entityStoreOrder.Count < 1)
+            bool status;
+            try
             {
-                Console.WriteLine("no orders placed at store");
-                entityStoreOrder.Add(new ViewStoreOrder());
-                entityStoreOrder[0].FirstName = "No Orders Placed at Store Location";
-                return entityStoreOrder;
+                _context.Stores.Add(store);
+                _context.SaveChanges();
+                status = true;
             }
-            else
+            catch (Exception)
             {
-                return entityStoreOrder;
+                status = false;
             }
+
+            return status;
+        }
+
+        public bool DeleteStore(Store store)
+        {
+            bool status;
+            try
+            {
+                Store deleteStore = _context.Stores.Find(store.StoreId);
+                if(deleteStore != null)
+                {
+                    _context.Stores.Remove(deleteStore);
+                    _context.SaveChanges();
+                    status = true;
+                }
+                else
+                {
+                    status = false;
+                }
+            }
+            catch (Exception)
+            {
+                status = false;
+            }
+
+            return status;
+        }
+
+        public bool UpdateStore(Store store)
+        {
+            bool status;
+            try
+            {
+                Store dbStore = _context.Stores.Find(store.StoreId);
+                if(dbStore != null)
+                {
+                    dbStore.Address = store.Address;
+                    _context.SaveChanges();
+                    status = false;
+                }
+                else
+                {
+                    status = true;
+                }
+            }
+            catch (Exception)
+            {
+                status = false;
+            }
+
+            return status;
+        }
+
+        //public List<ViewStoreOrder> StoreOrder()
+        //{
+        //    // convert to viewCustomers. This would be for formatting responsiblities
+        //    //for demo, just return the entire customer
+
+        //    List<ViewStoreOrder> entityStoreOrder = _dbContext.ViewStoreOrder.FromSqlInterpolated($"SELECT * from ViewStoreOrder").ToList();
+
+        //    //go add the entity model
+        //    //entity model added
+        //    //switch to getting an Inventory
+        //    Console.WriteLine(entityStoreOrder[0].OrderId);
+
+
+        //    return entityStoreOrder; 
+        //}
+
+        //public List<ViewStoreOrder> StoreOrder(int num)
+        //{
+        //    // convert to viewCustomers. This would be for formatting responsiblities
+        //    //for demo, just return the entire customer
+
+        //    List<ViewStoreOrder> entityStoreOrder = _dbContext.ViewStoreOrder.FromSqlInterpolated($"SELECT * from ViewStoreOrder where storeId = {num}").ToList();
+
+        //    //go add the entity model
+        //    //entity model added
+        //    //switch to getting an Inventory
+        //    if (entityStoreOrder.Count < 1)
+        //    {
+        //        Console.WriteLine("no orders placed at store");
+        //        entityStoreOrder.Add(new ViewStoreOrder());
+        //        entityStoreOrder[0].FirstName = "No Orders Placed at Store Location";
+        //        return entityStoreOrder;
+        //    }
+        //    else
+        //    {
+        //        return entityStoreOrder;
+        //    }
 
           
-        }
+        //}
 
-        public List<ViewStoreOrder> CustomerOrder(int c)
-        {
-            // convert to viewCustomers. This would be for formatting responsiblities
-            //for demo, just return the entire customer
+        //public List<ViewStoreOrder> CustomerOrder(int c)
+        //{
+        //    // convert to viewCustomers. This would be for formatting responsiblities
+        //    //for demo, just return the entire customer
 
-            List<ViewStoreOrder> entityStoreOrder = _dbContext.ViewStoreOrder.FromSqlInterpolated($"SELECT * from ViewStoreOrder where customerId = {c}").ToList();
+        //    List<ViewStoreOrder> entityStoreOrder = _dbContext.ViewStoreOrder.FromSqlInterpolated($"SELECT * from ViewStoreOrder where customerId = {c}").ToList();
 
-            //go add the entity model
-            //entity model added
-            //switch to getting an Inventory
-            //Console.WriteLine(entityStoreOrder[0].OrderId);
+        //    //go add the entity model
+        //    //entity model added
+        //    //switch to getting an Inventory
+        //    //Console.WriteLine(entityStoreOrder[0].OrderId);
 
 
-            return entityStoreOrder;
-        }
+        //    return entityStoreOrder;
+        //}
 
-        public List<ViewStoreOrder> StoreOrder(Customer c)
-        {
-            throw new NotImplementedException();
-        }
+        //public List<ViewStoreOrder> StoreOrder(Customer c)
+        //{
+        //    throw new NotImplementedException();
+        //}
     }//eoc
 }//eon
